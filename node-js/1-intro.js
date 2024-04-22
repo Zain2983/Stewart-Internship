c35cf3d2ec0495f79887a124e1a569edf5f0ef42
Zain Al Abidin <113837968+Zain2983@users.noreply.github.com>
2024-04-22 21:45:39 +0500

    Add files via upload

---
+++ node-js/1-intro.js
@@ -0,0 +1,13 @@
+const amount = 5;
+const x = 5
+
+if(amount<10){
+    console.log(`-----------------------------`)
+    console.log('the number is smaller than 12');
+    console.log(`-----------------------------`)
+}else{
+    console.log(`-----------------------------`)
+    console.log('the number is greater than 12');
+    console.log(`-----------------------------`)
+}
+
---
+++ node-js/4-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = 'Super Secret Phrase'
+// Share
+const john = `john`
+const peter = `peter`
+
+//console.log(module)
+module.exports = {john,peter}
+//console.log(module)
